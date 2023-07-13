da93c5f9a7572cbee03996a299a18e577a5a0502
Dhamotharan <70276832+dhamotharand347@users.noreply.github.com>
2023-07-13 12:09:15 +0530

    Update all_checks.py

--- all_checks.py
+++ all_checks.py
@@ -9,21 +9,21 @@ def check_reboot():
     """Returns True if the computer has a pending reboot."""
     return os.path.exists("/run/reboot-required")
 
-def check_disk_full(disk,min_gb,min_percent):
+def check_disk_full(disk,min_,min_percent):
     """Returns True if there isn't enough disk space, False otherwise."""
     du=shutil.disk_usage(disk)
     #calculate the persentage of free space
     percent_free = 100 * du.free / du.total
     #calculate how many free gigabytes
     gigabytes_free = du.free / 2**30
-    if gigabytes_free < min_gb or percent_free < min_percent:
+    if gigabytes_free < min_GB or percent_free < min_percent:
         return True
     return False
 
 
 def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
-    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+    return check_disk_full(disk="/", min_GB=2, min_percent=10)
 
 def check_cpu_constrained():
     """Returns True if the cpu is having too much usage, False otherwise."""
