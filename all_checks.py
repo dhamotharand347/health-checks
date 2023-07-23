adde61638804b9cfc024ab8f42e6be9b0d8b8bca
Dhamotharan <70276832+dhamotharand347@users.noreply.github.com>
2023-07-23 20:05:40 +0530

    Added cpu check function

--- all_checks.py
+++ all_checks.py
@@ -25,12 +25,16 @@ def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk="/", min_GB=2, min_percent=10)
 
+def check_cpu_constrained():
+    """Returns True if the cpu is having too much usage, False otherwise."""
+    return psutil.cpu_percent(1) > 75
+
 
 def main():
     checks=[
         (check_reboot, "Pending Reboot"),
         (check_root_full, "Root partition full"),
-        #(check_cpu_constrained, "CPU load too high."),
+        (check_cpu_constrained, "CPU load too high."),
         #(check_no_network, "No working network."),
     ]
     everything_ok= True
